6d57b835698f82e8fa0201b090bc44c78c4b669b
Sivuyile Magutywa <s.magutywa@applicafro.co.za>
2016-03-07 13:06:20 +0200

    added files

---
+++ Calculator_Simple/Calculator_Simple/WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Calculator_Simple.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Simple Calculator</div>
+        <br />
+    <asp:Label ID="txtN1" runat="server" Text="Number1 is "></asp:Label> 
+    <asp:TextBox ID="txtN"
+        runat="server"></asp:TextBox><br />
+    <asp:Label ID="txtN2" runat="server" Text="Number2 is "></asp:Label> 
+    <asp:TextBox ID="txtN22"
+        runat="server"></asp:TextBox>
+    <br /><br />
+    <asp:Button ID="btnCalc" runat="server" onclick="btnCalc_Click" 
+        Text="Calculate Total" />
+    <br />
+    <br />
+    <asp:Label ID="txtTotal" runat="server" Text="Total : 0"></asp:Label>
+    <br />
+    <br />
+        
+    </form>
+</body>
+</html>
---
+++ Calculator_Simple/Calculator_Simple/WebForm1.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator_Simple
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalc_Click(object sender, EventArgs e)
+        {
+            if (txtN.Text != "" && txtN22.Text != "")
+            {
+                try
+                {
+                    txtTotal.Text = txtN1.Text;
+
+                    int n1 = Int32.Parse(txtN.Text);
+                    int n2 = Int32.Parse(txtN22.Text);
+
+                    int total = n1 + n2;
+
+                    txtTotal.Text = "Total : " + total;
+                }
+                catch (FormatException)
+                {
+                    txtTotal.Text = "Invalid Input values";
+                }
+
+            }
+            else
+            {
+                txtTotal.Text = "Please make sure you the Form is filled in";
+            }
+        }
+    }
+}
